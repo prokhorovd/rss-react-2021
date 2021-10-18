d6e38187085491d8c0a5a6a87ff6f34cb108d16f
prokhorovd <github@prokhorov.dev>
2021-10-18 16:53:02 +0400

    feat: add css transition and refactor page rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import News from './components/News';
 import Header from './components/Header';
 
 const App = () => (
   <div>
     <h1 className="heading">News search engine</h1>
     <Header />
-    {/* <News /> */}
   </div>
 );
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,78 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   Link,
 } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { CSSTransition } from 'react-transition-group';
 import News from './News';
 import About from './About';
 import Page404 from './Page404';
-import { useLocation } from 'react-router';
 
-function RenderNav() {
+const routes = [
+  { path: '/', name: 'News', Component: News },
+  { path: '/about', name: 'About', Component: About },
+];
+
+function RenderNavigation() {
   const location = useLocation();
   const path = location.pathname;
-  console.log(location, typeof location, location.pathname);
   return (
     <nav>
-      <ul>
-        <li>
-          <Link className={path === '/' ? 'menu-link__active' : 'menu-link'} to="/">Home</Link>
-        </li>
-        <li>
-          <Link className={path === '/about' ? 'menu-link__active' : 'menu-link'} to="/about">About</Link>
-        </li>
-      </ul>
+      {routes.map((route) => (
+        <ul key={route.path}>
+          <li>
+            <Link
+              to={route.path}
+              className={path === route.path ? 'menu-link__active' : 'menu-link'}
+            >
+              {route.name}
+            </Link>
+          </li>
+        </ul>
+      ))}
     </nav>
+  );
+}
+
+function RenderContent() {
+  const location = useLocation();
+  if (location.pathname !== '/' && location.pathname !== '/about') {
+    return (
+      <div>
+        <Page404 />
+      </div>
+    );
+  }
+  return (
+    routes.map(({ path, Component }) => (
+      <Route key={path} exact path={path}>
+        {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={300}
+            classNames="page"
+            unmountOnExit
+          >
+            <div className="page">
+              <Component />
+            </div>
+          </CSSTransition>
+        )}
+      </Route>
+    ))
   );
 }
 
 function Header() {
   return (
-    <header>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              {/*<li style={location ? { color: 'red' } : { color: 'green' }}>*/}
-              {/*<li>*/}
-              {/*  <Link to="/">Home</Link>*/}
-              {/*</li>*/}
-              {/*<li>*/}
-              {/*  <Link to="/about">About</Link>*/}
-              {/*</li>*/}
-              <RenderNav />
-            </ul>
-          </nav>
-        </div>
-        {/* switch part */}
-        <Switch>
-          <Route exact path="/">
-            <News />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="*">
-            <Page404 />
-          </Route>
-        </Switch>
-      </Router>
-    </header>
+    <Router>
+      <nav>
+        <RenderNavigation />
+      </nav>
+      <RenderContent />
+    </Router>
   );
 }
 
